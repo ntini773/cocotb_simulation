5ca3d09726c1c66fc712a9cfa5de927c72bd6251
ntini773 <grandhi.sai@research.iiit.ac.in>
2025-08-03 01:28:23 +0530

    gitignore changed

--- top_tracing_simulation/sim_build/Vtop__Syms.d
+++
@@ -1,4 +0,0 @@
-Vtop__Syms.o: Vtop__Syms.cpp Vtop__pch.h Vtop.h Vtop___024root.h \
- Vtop_prim_cipher_pkg.h Vtop_prim_secded_pkg.h Vtop_ibex_tracer_pkg.h \
- Vtop_prim_mubi_pkg.h Vtop_prim_ram_2p_pkg.h Vtop_ibex_pkg.h \
- Vtop_prim_ram_1p_pkg.h
--- top_tracing_simulation/sim_build/Vtop_ibex_pkg__Slow.d
+++
@@ -1,2 +0,0 @@
-Vtop_ibex_pkg__Slow.o: Vtop_ibex_pkg__Slow.cpp Vtop__pch.h Vtop__Syms.h \
- Vtop_ibex_pkg.h
